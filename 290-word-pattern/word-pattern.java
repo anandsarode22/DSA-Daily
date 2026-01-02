78f2bd51793217f4efff094d61b101f8394bf159
anandsarode22 <anandsarode2004@gmail.com>
2026-01-02 13:28:36 +0530

    Time: 1 ms (80.81%) | Memory: 43 MB (16.46%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        // Length mismatch → cannot follow pattern
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            // Check character → word mapping
+            if (charToWord.containsKey(ch)) {
+                if (!charToWord.get(ch).equals(word)) {
+                    return false;
+                }
+            } else {
+                charToWord.put(ch, word);
+            }
+
+            // Check word → character mapping
+            if (wordToChar.containsKey(word)) {
+                if (wordToChar.get(word) != ch) {
+                    return false;
+                }
+            } else {
+                wordToChar.put(word, ch);
+            }
+        }
+
+        return true;
+    }
+}
